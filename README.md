39a27b83cf2d66295aae3583e904bce516f98abc
Eknoor Oberoi <38902676+eknooroberoi@users.noreply.github.com>
2020-06-16 23:02:07 +0530

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# React-Emojipedia
-Created with CodeSandbox
---
+++ src/emojipedia.js
@@ -0,0 +1,46 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+  {
+    id: 4,
+    emoji: "🙃",
+    name: "Upside-Down Face",
+    meaning:
+      "Don't take me seriously! The message is either ambiguous, ironic or joking. Person plays the fool and horses around, or in response to a joke."
+  },
+  {
+    id: 5,
+    emoji: "😖",
+    name: "Confounded Face",
+    meaning:
+      "How on earth could that happen? Smiley is extremely dismayed and bewildered. Or damn angry: That's almost the final straw!"
+  },
+  {
+    id: 6,
+    emoji: "🤓",
+    name: "Nerd Face",
+    meaning:
+      "Huge glasses, awkward smile and buck teeth. Used humorously or ironically for nerds or to express how smart you are. Stereotype of a nerd; a smart but funny-dressed person with social deficits."
+  }
+];
+
+export default emojipedia;
